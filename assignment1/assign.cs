d8d3d818483d5d156aa85d1372e3f4b646fe225a
tysonfaver <tyson.faver@gmail.com>
2019-09-11 02:14:01 +0000

    note to self

---
+++ assignment1/assign.cs
@@ -0,0 +1,18 @@
+using System;
+class Pizza problem {
+  public static void Main () {
+    Console.WriteLine ("Please input number of people in your party.");
+    
+    int numOfPeeps, numNedSlice, numOfNedPiz, 
+
+      numOfPeeps = Console.ReadLine()
+
+      numNedSlice = (numOfPeeps) * 2
+ 
+      numOfNedPiz = ()
+ 
+ 
+ 
+ 
+  }
+}
